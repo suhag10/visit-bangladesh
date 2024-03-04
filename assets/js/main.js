3f65213eef39538fa48e93c72c7a49a1b95a179e
suhag10 <suhag78641@gmail.com>
2024-03-05 01:25:03 +0600

    Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,19 @@
 // Get the button:
-let topbutton = document.getElementById("topBtn");
+let btn = document.getElementById("top-btn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {isShow()};
 
-function scrollFunction() {
+function isShow() {
   if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    topbutton.style.display = "block";
+    btn.style.display = "block";
   } else {
-    topbutton.style.display = "none";
+    btn.style.display = "none";
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function getTop() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
